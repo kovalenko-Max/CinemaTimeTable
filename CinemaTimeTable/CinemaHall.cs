1b0a8ede2377499c46168a2077a8b8292010faf5
Maks Kovalenko <max.don@live.ru>
2021-05-03 15:35:57 +0300

    Init

---
+++ CinemaTimeTable/CinemaHall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTimeTable
+{
+    public class CinemaHall
+    {
+        public TimeSpan WorkDayDuration { get; set; }
+
+        public CinemaHall(TimeSpan workDayDuration)
+        {
+            WorkDayDuration = workDayDuration;
+        }
+    }
+}
---
+++ CinemaTimeTable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CinemaTimeTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Cinema cinema = new Cinema();
+        }
+    }
+}
